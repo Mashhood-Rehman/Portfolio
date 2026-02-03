822fa2151130ec2184203077b6a3feaca4304310
Mashhood-Rehman <mashhoodbutt47@gmail.com>
2026-02-02 21:58:14 -0800

    added

--- components/intro.tsx
+++ components/intro.tsx
@@ -62,7 +62,7 @@ export default function Intro() {
       >
         <span className="font-bold">Hello, I&apos;m Mashhood.</span> I&apos;m a
         <span className="font-bold"> full-stack developer</span> with
-        <span className="font-bold"> 1.5 years</span> of experience. I enjoy
+        <span className="font-bold"> 2 years</span> of experience. I enjoy
         building <span className="italic">sites & apps</span>. My focus is
         <span className="underline"> MERN</span>.
       </motion.h1>
@@ -96,8 +96,8 @@ export default function Intro() {
         </a>
         <a
           className="bg-white px-4 py-2 ml-2 text-black hover:text-gray-950 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
-          href="/Resume_Mashhood.pdf"
-          download="Resume_Mashhood.pdf"
+          href="/Resume_MASHHOOD.pdf"
+          download="Resume_MASHHOOD.pdf"
         >
           Download CV
         </a>
